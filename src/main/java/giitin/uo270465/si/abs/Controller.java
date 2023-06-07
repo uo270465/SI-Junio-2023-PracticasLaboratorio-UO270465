8d7f3f0dac5c9329361a7fc2a94c5875c5d490cf
uo270465 <uo270465@uniovi.es>
2023-06-08 01:16:05 +0200

    Añadido fecha como pampo estatico

--- src/main/java/giitin/uo270465/si/abs/Controller.java
+++ src/main/java/giitin/uo270465/si/abs/Controller.java
@@ -1,12 +1,16 @@
 package giitin.uo270465.si.abs;
 
+import java.util.Date;
+
 public class Controller<M extends Model, V extends View> {
 	
 	protected M model;
 	protected V view;
+	protected Date fecha;
 	
-	public Controller(M model, V view) {
+	public Controller(M model, V view, Date fecha) {
 		this.model = model;
 		this.view = view;
+		this.fecha = fecha;		
 	}
 }
