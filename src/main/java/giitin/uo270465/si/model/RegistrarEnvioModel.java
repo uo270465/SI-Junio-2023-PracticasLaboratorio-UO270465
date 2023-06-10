a00f52f379eb463b9e0b2f5520a97e1743244cca
uo270465 <uo270465@uniovi.es>
2023-06-10 21:02:57 +0200

    Añadido metodo en el modelo para obtener todos los almacenes u oficinas

--- src/main/java/giitin/uo270465/si/model/RegistrarEnvioModel.java
+++ src/main/java/giitin/uo270465/si/model/RegistrarEnvioModel.java
@@ -3,6 +3,7 @@ package giitin.uo270465.si.model;
 import java.util.List;
 
 import giitin.uo270465.si.abs.Model;
+import giitin.uo270465.si.dto.AlmacenOficinaDTO;
 import giitin.uo270465.si.dto.ClienteDTO;
 
 public class RegistrarEnvioModel extends Model {
@@ -12,12 +13,17 @@ public class RegistrarEnvioModel extends Model {
 	}
 
 	public List<ClienteDTO> getClientes() {
-		final String QUERY = "SELECT * FROM clientes";
+		final String QUERY = "SELECT * FROM Clientes";
 		return db.executeQueryPojo(ClienteDTO.class, QUERY);
 	}
 
 	public List<ClienteDTO> getClientesFilterAnyColumn(String filter) {
-		final String QUERY = "SELECT * FROM clientes WHERE nombre LIKE '%"+filter+"%' OR email LIKE '%"+filter+"%' OR direccion LIKE '%"+filter+"%'";
+		final String QUERY = "SELECT * FROM Clientes WHERE nombre LIKE '%"+filter+"%' OR email LIKE '%"+filter+"%' OR direccion LIKE '%"+filter+"%'";
 		return db.executeQueryPojo(ClienteDTO.class, QUERY);
+	}
+	
+	public List<AlmacenOficinaDTO> getAlmacenesOficinas(){
+		final String QUERY = "SELECT * FROM AlmacenesOficinas";
+		return db.executeQueryPojo(AlmacenOficinaDTO.class, QUERY);
 	}
 }
