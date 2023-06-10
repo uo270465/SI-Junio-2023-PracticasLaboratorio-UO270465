bf7b377b97834060ecf0ea30184f82f9a12ce97b
uo270465 <uo270465@uniovi.es>
2023-06-10 18:17:05 +0200

    Refactorización de los MVC

--- src/main/java/giitin/uo270465/si/model/RegistrarEnvioModel.java
+++ src/main/java/giitin/uo270465/si/model/RegistrarEnvioModel.java
@@ -5,13 +5,13 @@ import java.util.List;
 import giitin.uo270465.si.abs.Model;
 import giitin.uo270465.si.dto.ClienteDTO;
 
-public class RegistrarEnvioModel extends Model{
-	
+public class RegistrarEnvioModel extends Model {
+
 	public RegistrarEnvioModel() {
 		super();
 	}
-	
-	public List<ClienteDTO> getClientes(){
+
+	public List<ClienteDTO> getClientes() {
 		final String QUERY = "SELECT * FROM clientes";
 		return db.executeQueryPojo(ClienteDTO.class, QUERY);
 	}
--- src/main/java/giitin/uo270465/si/view/RegistrarEnvioView.java
+++ src/main/java/giitin/uo270465/si/view/RegistrarEnvioView.java
@@ -12,40 +12,35 @@ import javax.swing.table.DefaultTableModel;
 import giitin.uo270465.si.abs.View;
 import net.miginfocom.swing.MigLayout;
 
-public class RegistrarEnvioView extends View{
+public class RegistrarEnvioView extends View {
 	private static final long serialVersionUID = 1L;
 	private JTable tClientes;
+
 	public RegistrarEnvioView() {
 		getContentPane().setLayout(new BorderLayout(0, 0));
-		
+
 		JScrollPane scrollPane_1 = new JScrollPane();
 		getContentPane().add(scrollPane_1);
-		
+
 		JPanel panel = new JPanel();
 		scrollPane_1.setViewportView(panel);
 		panel.setLayout(new MigLayout("", "[grow][grow]", "[][][grow]"));
-		
+
 		JLabel lblNewLabel = new JLabel("Seleccione una opción:");
 		panel.add(lblNewLabel, "cell 0 0 2 1");
-		
+
 		JToggleButton tglbtnNewToggleButton = new JToggleButton("Nuevo cliente");
 		panel.add(tglbtnNewToggleButton, "flowx,cell 0 1,growx");
-		
+
 		JToggleButton tglbtnNewToggleButton_1 = new JToggleButton("Cliente Existente");
 		panel.add(tglbtnNewToggleButton_1, "cell 1 1,growx");
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		panel.add(scrollPane, "cell 1 2,grow");
-		
+
 		tClientes = new JTable();
-		tClientes.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null},
-			},
-			new String[] {
-				"Nombre", "Email", "Direcci\u00F3n"
-			}
-		));
+		tClientes.setModel(new DefaultTableModel(new Object[][] { { null, null, null }, },
+				new String[] { "Nombre", "Email", "Direcci\u00F3n" }));
 		tClientes.getColumnModel().getColumn(0).setPreferredWidth(51);
 		tClientes.getColumnModel().getColumn(1).setPreferredWidth(40);
 		tClientes.getColumnModel().getColumn(2).setPreferredWidth(58);
